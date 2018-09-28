46d1dcaf0ea65a10aeeb95a94b0ef01d173838e2
blokyk <eliot.courvoisier@gmail.com>
2018-09-28 23:41:50 +0200

    Still experimenting, sorry

--- addncommit.bat
+++ addncommit.bat
@@ -1,4 +1,6 @@
 git add .
 git commit
+git reset eclipse/*metadata/*
+git reset eclipse/*reco*/*
 git push
 git pull
--- addncommit.sh
+++ addncommit.sh
@@ -2,5 +2,8 @@
 
 git add .
 git commit
+git reset eclipse/*metadata/*
+git reset eclipse/*reco*/*
 git push
 git pull
+
